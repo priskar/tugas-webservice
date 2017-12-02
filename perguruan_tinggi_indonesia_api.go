8241577551e5e9b41a2da7cebf6ea54e3927d42f
priskar <priska_reysa@yahoo.com>
2017-12-02 10:55:58 +0700

    add type perguruanTinggiIndonesia & perguruanTinggiDiIndonesia

--- perguruan_tinggi_indonesia_api.go
+++ perguruan_tinggi_indonesia_api.go
@@ -6,12 +6,26 @@ import (
         "fmt"
         "log"
         "net/http"
+        "strconv"
 
         _ "github.com/go-sql-driver/mysql"
 )
 
-type perguruanTinggi struct{
+type perguruanTinggiDiIndonesia struct{
         Id_perguruan_tinggi     int
+        Nama_perguruan_tinggi   string
+        Alamat                  string
+        Provinsi                string
+        No_telepon              string
+}
+
+type perguruanTinggiIndonesia struct{
+        Nama_perguruan_tinggi   string
+        Alamat                  string
+        No_telepon              string
+}
+
+type perguruanTinggi struct{
         Nama_perguruan_tinggi   string
         Alamat                  string
         Provinsi                string
@@ -29,7 +43,7 @@ func main() {
         http.HandleFunc("/perguruan_tinggi/", func(w http.ResponseWriter, r *http.Request) {
                 switch r.Method {
                         case "GET":
-                                s:=r.URL.Query().Get("provinsi")
+                                s:=r.URL.Query().Get("id")
                                 if s!=""{
                                         GetPerguruanTinggiByProvinsi(w, r, s)
                                 } else {
@@ -89,37 +103,6 @@ func GetAllPerguruanTinggi(w http.ResponseWriter, r *http.Request) {
         }
 }
 
-//GetAllRektor
-func GetAllRektor(w http.ResponseWriter, r *http.Request) {
-        db, err := sql.Open("mysql",
-                "root:@tcp(127.0.0.1:3306)/perguruan_tinggi_indonesia")
-
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer db.Close()
-
-        rektor := rektor{}
-
-        rows, err:=db.Query("select Id_rektor, Nama_rektor, Email from rektor")
-        if err != nil {
-                log.Fatal(err)
-        }
-
-        defer rows.Close()
-        for rows.Next() {
-                err:= rows.Scan(&rektor.Id_rektor, &rektor.Nama_rektor, &rektor.Email)
-                if err != nil{
-                        log.Fatal(err)
-                }
-                json.NewEncoder(w).Encode(&rektor)
-        }
-        err=rows.Err()
-        if err != nil {
-                log.Fatal(err)
-        }
-}
-
 //GetPerguruanTinggiByProvinsi
 func GetPerguruanTinggiByProvinsi(w http.ResponseWriter, r *http.Request, provinsi string) {
         db, err := sql.Open("mysql",
@@ -144,6 +127,37 @@ func GetPerguruanTinggiByProvinsi(w http.ResponseWriter, r *http.Request, provin
                         log.Fatal(err)
                 }
                 json.NewEncoder(w).Encode(&perguruan_tinggi)
+        }
+        err=rows.Err()
+        if err != nil {
+                log.Fatal(err)
+        }
+}
+
+//GetAllRektor
+func GetAllRektor(w http.ResponseWriter, r *http.Request) {
+        db, err := sql.Open("mysql",
+                "root:@tcp(127.0.0.1:3306)/perguruan_tinggi_indonesia")
+
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer db.Close()
+
+        rektor := rektor{}
+
+        rows, err:=db.Query("select Id_rektor, Nama_rektor, Email from rektor")
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        defer rows.Close()
+        for rows.Next() {
+                err:= rows.Scan(&rektor.Id_rektor, &rektor.Nama_rektor, &rektor.Email)
+                if err != nil{
+                        log.Fatal(err)
+                }
+                json.NewEncoder(w).Encode(&rektor)
         }
         err=rows.Err()
         if err != nil {
